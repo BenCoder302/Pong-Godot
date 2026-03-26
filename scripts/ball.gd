f1baf4897961f34a311a163cf07bd7493dc52e49
BenCoder302 <bencoder302@gmail.com>
2026-03-26 16:43:10 +0530

    Added player and enemy scores

--- scripts/ball.gd
+++ scripts/ball.gd
@@ -2,17 +2,33 @@ extends Area2D
 
 @export var speed_x = 100
 @export var speed_y = 0
+@onready var timer = $BallTimer
 
-func _ready():
+func reset_ball():
 	position.x = 320
 	position.y = 240
 
+func wait():
+	timer.start()
+
+func _ready():
+	reset_ball()
+	
 func move_ball(delta):
 	position.x += speed_x * delta
 	position.y += speed_y * delta
 
 func _process(delta):
 	move_ball(delta)
+	check_score()
 
 func _on_area_entered(area: Area2D) -> void:
 	speed_x = -speed_x
+
+func check_score():
+	if position.x <= 0:
+		GameState.increment_enemy_score()
+		reset_ball()
+	if position.x >= 640:
+		GameState.increment_player_score()
+		reset_ball()
---
+++ scripts/game_state.gd
@@ -0,0 +1,10 @@
+extends Node
+
+var player_score = 0
+var enemy_score = 0
+
+func increment_player_score():
+	player_score += 1
+	
+func increment_enemy_score():
+	enemy_score += 1
